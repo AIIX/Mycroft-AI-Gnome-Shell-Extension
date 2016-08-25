6f7b3819acf71fada7588dedee4181e75d237265
Aditya Mehra <aix.m@outlook.com>
2016-08-26 03:39:02 +0930

    Delete MycroftServiceStart.sh

--- mycroftaignome@aix.com/MycroftServiceStart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-workon mycroft
-cd /home/$USER/mycroft-core-master/
-espeak "Mycroft Ai Is Starting.... Please Wait"
-./start.sh service & ./start.sh skills & ./start.sh voice sleep 10 && espeak "Mycroft Ai Service Has Started"
-
