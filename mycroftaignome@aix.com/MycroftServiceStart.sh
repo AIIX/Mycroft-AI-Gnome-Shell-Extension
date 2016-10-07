3c0e1d2da825285eed762e624bdf6af5cdc2974a
Aix <aix.m@outlook.com>
2016-10-08 02:00:28 +1030

    Bug Fix Version 0.1.3

--- mycroftaignome@aix.com/MycroftServiceStart.sh
+++ mycroftaignome@aix.com/MycroftServiceStart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 workon mycroft
-cd /home/$USER/mycroft-core-master/
+cd /home/$USER/mycroft-core*/
 ./mycroft.sh start
 espeak -ven+m4 "Initialized"
 python /home/$USER/.local/share/gnome-shell/extensions/mycroftaignome@aix.com/Connectmycroft.py
